1f44b1b2b028154bdc027dbb17e715e4436a2d91
wuan.duc <univerbachtuoc@gmail.com>
2024-01-09 23:18:42 +0700

    add some file

--- router/router.js
+++ router/router.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const { getAllProductTypes } = require("../controllers/ProductType");
 const {
+  getSaleProductById,
+  deleteSaleProduct,
+  updateSaleProduct,
+  addSaleProduct,
+  getAllSaleProducts,
+} = require("../controllers/SaleProduct");
+const {
   getStaffById,
   deleteStaff,
   updateStaff,
@@ -11,7 +18,12 @@ const router = express.Router();
 
 //ProductType
 router.get("/ProductType/getProductTypes", getAllProductTypes);
-
+//Sale Product
+router.get("/SaleProduct/getSaleProduct", getAllSaleProducts);
+router.post("/SaleProduct/add", addSaleProduct);
+router.put("/SaleProduct/update/:saleProductId", updateSaleProduct);
+router.delete("/SaleProduct/delete/:saleProductId", deleteSaleProduct);
+router.get("/SaleProduct/saleProductById/:Id", getSaleProductById);
 //Staff
 router.get("/StaffManagement/getStaffs", getAllStaffs);
 router.post("/StaffManagement/add", addStaff);
